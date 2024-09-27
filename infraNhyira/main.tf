983729601cbc3ef2de66f68f07df08a5aed07248
WillDantasJPG <willgustavodantasadolpho@gmail.com>
2024-09-26 22:06:18 -0300

    feature: subindo webscoket

--- infraNhyira/main.tf
+++ infraNhyira/main.tf
@@ -1,74 +1,19 @@
-resource "aws_instance" "websocket_ec2_01" {
+resource "aws_instance" "public_ec2_01" {
   ami               = var.ami
   availability_zone = var.az
   instance_type     = var.inst_type
-
   ebs_block_device {
     device_name = "/dev/sda1"
     volume_size = 8
     volume_type = "gp3"
   }
-
-  key_name                    = var.key_pair_name  # Ajuste aqui para usar a variável
+  key_name                    = "shh_key"
   subnet_id                   = var.subnet_id
   associate_public_ip_address = true
   vpc_security_group_ids      = [var.sg_id]
-
   tags = {
-    Name = "websocket_ec2_01"
+    Name = "public_ec2_01"
   }
-
-  user_data = <<-EOF
-    #!/bin/bash
-
-    # Atualizar pacotes
-    sudo apt-get update -y
-
-    # Instalar Node.js e npm
-    sudo apt-get install -y curl
-    curl -sL https://deb.nodesource.com/setup_16.x | sudo -E bash -
-    sudo apt-get install -y nodejs
-
-    # Instalar PM2 para gerenciamento do processo Node.js
-    sudo npm install -g pm2
-
-    # Clonar o repositório
-    git clone https://github.com/nhyira-group5/websocket.git /home/ubuntu/websocket
-
-    # Navegar até o diretório do repositório clonado
-    cd /home/ubuntu/websocket
-
-    # Instalar dependências
-    npm install --force
-
-    # Ajustar permissões
-    sudo chown -R ubuntu:ubuntu /home/ubuntu/websocket
-
-    # Iniciar a aplicação com PM2
-    pm2 start server.js --name websocket-app
-  EOF
-}
-
-resource "aws_instance" "websocket_ec2_02" {
-  ami               = var.ami
-  availability_zone = var.az
-  instance_type     = var.inst_type
-
-  ebs_block_device {
-    device_name = "/dev/sda1"
-    volume_size = 8
-    volume_type = "gp3"
-  }
-
-  key_name                    = var.key_pair_name  # Ajuste aqui para usar a variável
-  subnet_id                   = var.subnet_id
-  associate_public_ip_address = true
-  vpc_security_group_ids      = [var.sg_id]
-
-  tags = {
-    Name = "websocket_ec2_02"
-  }
-
   user_data = <<-EOF
     #!/bin/bash
 
@@ -85,10 +30,10 @@ resource "aws_instance" "websocket_ec2_02" {
     sudo apt-get install -y nodejs npm
 
     # Clonar o repositório
-    git clone https://github.com/Food-Way/Web.git
+    git clone https://github.com/nhyira-group5/websocket.git
 
     # Navegar até o diretório do repositório clonado
-    cd /home/ubuntu/Web/foodway
+    cd /home/ubuntu/Web/websocket
 
     # Instalar dependências
     sudo npm i --force
@@ -110,6 +55,56 @@ resource "aws_instance" "websocket_ec2_02" {
   EOF
 }
 
+resource "aws_instance" "public_ec2_02" {
+  ami               = var.ami
+  availability_zone = var.az
+  instance_type     = var.inst_type
+  ebs_block_device {
+    device_name = "/dev/sda1"
+    volume_size = 8
+    volume_type = "gp3"
+  }
+  key_name                    = "shh_key"
+  subnet_id                   = var.subnet_id
+  associate_public_ip_address = true
+  vpc_security_group_ids      = [var.sg_id]
+  tags = {
+    Name = "public_ec2_02"
+  }
+  user_data = <<-EOF
+    #!/bin/bash
+
+    # Atualizar pacotes
+    sudo apt-get update
+
+    # Adicionar o usuário 'ubuntu' ao grupo sudo
+    sudo usermod -aG sudo ubuntu
+    
+    # Ajustar permissões
+    sudo chown -R ubuntu:ubuntu /var/www
+
+    # Instalar Node.js e npm
+    sudo apt-get install -y nodejs npm
+
+    # Clonar o repositório
+    git clone https://github.com/Food-Way/Web.git
+
+    # Navegar até o diretório do repositório clonado
+    cd /home/ubuntu/Web/foodway
+
+    # Instalar dependências
+    sudo npm i --force
+
+    # Executar build
+    sudo npm run build
+
+    # Copiar o diretório 'dist' para a pasta específica
+    sudo cp -r dist /var/www
+
+    # Restartando nginx
+    sudo systemctl restart nginx
+  EOF
+}
 
 resource "aws_eip_association" "eip_assoc_01" {
   instance_id   = aws_instance.websocket_ec2_01.id
@@ -118,5 +113,5 @@ resource "aws_eip_association" "eip_assoc_01" {
 
 resource "aws_eip_association" "eip_assoc_02" {
   instance_id   = aws_instance.websocket_ec2_02.id
-  allocation_id = "eipalloc-01952682e36b66c07" 
+  allocation_id = "eipalloc-01952682e36b66c07" 
 }

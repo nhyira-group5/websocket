e0de1e440c5b6a207304ce4374b358173b166c1e
WillDantasJPG <willgustavodantasadolpho@gmail.com>
2024-10-01 23:44:23 -0300

    feature:websocket

--- routes/user.js
+++ routes/user.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const { sql, poolPromise } = require('../database/sqlserver');
+const { sql, poolPromise } = require('../database/mysql');
 
 router.get('/', async (req, res) => {
     try {

c0105cfca07e406f57b387c7b8f2c4030e1de94d
Will Gustavo Dantas <71149012+WillDantasJPG@users.noreply.github.com>
2024-10-08 07:38:35 -0300

    feature:Mysql

--- database/mysql.js
+++ database/mysql.js
@@ -1,22 +1,21 @@
 const mysql = require('mysql2/promise');
 require('dotenv').config();
 
-// Defina a configuração do MySQL usando variáveis de ambiente
 const config = {
-    host: process.env.MYSQL_HOST, // endereço do servidor MySQL
-    user: process.env.MYSQL_USER, // usuário do MySQL
-    password: process.env.MYSQL_PASSWORD, // senha do MySQL
-    database: process.env.MYSQL_DATABASE, // nome do banco de dados
+    host: process.env.MYSQL_HOST, // Deve ser o IP privado
+    user: process.env.MYSQL_USER,
+    password: process.env.MYSQL_PASSWORD,
+    database: process.env.MYSQL_DATABASE,
     waitForConnections: true,
     connectionLimit: 10,
     queueLimit: 0
 };
 
-// Crie a pool de conexões
-const poolPromise = mysql.createPool(config); // Não usa `then` aqui
-console.log('Connected to MySQL');
-
-module.exports = {
-    mysql,
-    poolPromise
-};
+const poolPromise = mysql.createPool(config)
+    .then(pool => {
+        console.log('Conectado ao MySQL');
+        return pool;
+    })
+    .catch(err => {
+        console.error('Erro ao conectar ao MySQL:', err);
+    });

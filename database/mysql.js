77059ef062cc29dc3dadfb6a6fe693a51e61326d
Will Gustavo Dantas <71149012+WillDantasJPG@users.noreply.github.com>
2024-10-11 09:29:49 -0300

    Update mysql.js

--- database/mysql.js
+++ database/mysql.js
@@ -1,21 +1,30 @@
 const mysql = require('mysql2/promise');
 require('dotenv').config();
 
-const config = {
-    host: process.env.MYSQL_HOST, // Deve ser o IP privado
-    user: process.env.MYSQL_USER,
-    password: process.env.MYSQL_PASSWORD,
-    database: process.env.MYSQL_DATABASE,
-    waitForConnections: true,
-    connectionLimit: 10,
-    queueLimit: 0
-};
+let pool;
 
-const poolPromise = mysql.createPool(config)
-    .then(pool => {
-        console.log('Conectado ao MySQL');
-        return pool;
-    })
-    .catch(err => {
+async function initializePool() {
+    try {
+        pool = mysql.createPool({
+            host: process.env.MYSQL_HOST,       // IP ou hostname do servidor MySQL
+            user: process.env.MYSQL_USER,       // Nome de usuário do MySQL
+            password: process.env.MYSQL_PASSWORD,// Senha do MySQL
+            database: process.env.MYSQL_DATABASE,// Nome do banco de dados
+            waitForConnections: true,            // Espera por conexões
+            connectionLimit: 10,                 // Limite de conexões
+            queueLimit: 0                         // Sem limite de fila
+        });
+        console.log('Pool de conexões criado com sucesso.');
+    } catch (err) {
         console.error('Erro ao conectar ao MySQL:', err);
-    });
+        throw err; // Lançar erro para que a aplicação não continue se a conexão falhar
+    }
+}
+
+// Chame a função de inicialização do pool
+initializePool();
+
+// Exportar o pool para ser usado em outros arquivos
+module.exports = {
+    getPool: () => pool,
+};
